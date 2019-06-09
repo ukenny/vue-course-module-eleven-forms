926404d4946c4aaf2580b2cbf8d3f53e3d634cff
Kenny Urena <urenakenny@outlook.com>
2019-06-09 09:04:24 -0400

    Refactored code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,172 @@
 <template>
-  <div id="app"></div>
+  <b-container>
+    <b-form>
+      <b-form-row>
+        <h1>File a Complaint</h1>
+        <hr />
+      </b-form-row>
+
+      <b-form-group
+        id="fieldset-horizontal-1"
+        label-cols-sm="2"
+        description="Please provide your email address."
+        label="Enter your email:"
+        label-for="email"
+      >
+        <b-form-input id="emal"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="fieldset-horizontal-2"
+        label-cols-sm="2"
+        description="Please provide your name."
+        label="Enter your name:"
+        label-for="name"
+      >
+        <b-form-input id="name"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="fieldset-horizontal-3"
+        label-cols-sm="2"
+        description="Please provide your age."
+        label="Enter your age:"
+        label-for="age"
+      >
+        <b-form-input type="number" id="age"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="fieldset-horizontal-4"
+        label-cols-sm="2"
+        description="Please provide a escription of the problem."
+        label="Message:"
+        label-for="message"
+      >
+        <b-textarea
+          maxlength="200"
+          rows="2"
+          max-rows="3"
+          no-resize
+          placeholder="My problem is that..."
+          id="message"
+        ></b-textarea>
+      </b-form-group>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <b-form-group>
+          <b-form-checkbox
+            id="sendemail"
+            v-model="sendEmail"
+            name="SendMail"
+            value="send_email"
+            unchecked-value="do_not_send_email"
+            >Send Mail</b-form-checkbox
+          >
+          <b-form-checkbox
+            id="sendInfoMail"
+            v-model="sendInfoMail"
+            name="SendInfoMail"
+            value="send_info_mail"
+            unchecked-value="do_not_send_info_mail"
+            >Send Info Mail</b-form-checkbox
+          >
+        </b-form-group>
+      </div>
+      <b-form-row>
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <b-form-group label="Select your gender:">
+            <b-form-radio-group
+              id="gender-radio-selection"
+              v-model="genderSelection"
+              :options="options"
+              name="gender-selection"
+            ></b-form-radio-group>
+          </b-form-group>
+        </div>
+        <div class="col-xs-3" style="text-align:right;">
+          <b-dropdown
+            id="priority"
+            variant="primary"
+            text="Priority Level"
+            class="m-md-2"
+          >
+            <b-dropdown-item @click="priorityClicked($event)" active
+              >Low</b-dropdown-item
+            >
+            <b-dropdown-item @click="priorityClicked($event)"
+              >Medium</b-dropdown-item
+            >
+            <b-dropdown-item @click="priorityClicked($event)"
+              >High</b-dropdown-item
+            >
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item @click="priorityClicked($event)"
+              >Critical</b-dropdown-item
+            >
+          </b-dropdown>
+          <div class="col-xs-2">
+            <p>
+              Current Priority Level:
+              <i>{{ priorityLevel }}</i>
+            </p>
+          </div>
+        </div>
+      </b-form-row>
+      <div class="col-xs-12" style="text-align:center;">
+        <button class="btn btn-success">Submit!</button>
+      </div>
+      <hr />
+      <b-form-row>
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4>Your Data</h4>
+            </div>
+            <div class="panel-body">
+              <p>Mail:</p>
+              <p>Password:</p>
+              <p>Age:</p>
+              <p>Message:</p>
+              <p>
+                <strong>Send Mail?</strong>
+              </p>
+              <ul>
+                <li></li>
+              </ul>
+              <p>Gender:</p>
+              <p>Priority:</p>
+              <p>Switched:</p>
+            </div>
+          </div>
+        </div>
+      </b-form-row>
+    </b-form>
+  </b-container>
 </template>
 
 <script>
 export default {
   name: "app",
-  components: {}
+  components: {},
+  data: function() {
+    return {
+      priorityLevel: "Low",
+      sendEmail: "do_not_send_email",
+      sendInfoMail: "do_not_send_info_mail",
+      genderSelection: "other",
+      options: [
+        { text: "Male", value: "male" },
+        { text: "Female", value: "female" },
+        { text: "Prefer Not To Say", value: "other" }
+      ]
+    };
+  },
+  methods: {
+    priorityClicked(event) {
+      this.priorityLevel = event.target.text;
+    }
+  }
 };
 </script>
 
@@ -17,5 +178,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input[type="number"] {
+  -webkit-appearance: textfield;
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
 }
 </style>
